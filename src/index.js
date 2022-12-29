3194833f4428371141e606145597b7ef22742045
Prajwol Shrestha <prajwol.shrestha18@gmail.com>
2022-12-29 14:47:03 +0545

    3 fix2,copy pasted code from instructor because they werent accepting solution

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-const movies = require('./data/movies.json');
-import { fetchWithTimeout } from './services';
-export function fetchMovies(){
+import { fetchWithTimeout } from "./services";
+const movies = require("./data/movies.json");
+
+export function fetchMovies() {
     const resolveFunction = () => movies;
-    return fetchWithTimeout(1000).then(resolveFunction)
+    return fetchWithTimeout(1000).then(resolveFunction);
 }
 
 const moviePromise = fetchMovies();
-moviePromise.then((results)=>{
+moviePromise.then(results => {
     console.log(results);
 });
