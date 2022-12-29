5bf60a4edc8b0f4512440101b731ebbc356776be
Prajwol Shrestha <prajwol.shrestha18@gmail.com>
2022-12-29 14:38:52 +0545

    3, created promise and logged the movies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-export function fetchMovies(){}
+const movies = require('./somefile.json')
+const services = require('./services')
+
+export function fetchMovies(){
+    const resolveFunction = () => movies;
+    return services.fetchWithTimeout(1000).then(resolveFunction)
+}
+
+const moviePromise = fetchMovies();
+moviePromise.then((results)=>{
+    console.log(results);
+})
